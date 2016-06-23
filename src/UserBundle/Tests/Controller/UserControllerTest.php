8dba85ee463116e1fa11d815b4245b9710b1d679
Tomáš Fejfar <tomas.fejfar@gmail.com>
2016-06-23 18:49:56 +0200

    Getting the tests ready

--- src/UserBundle/Tests/Controller/UserControllerTest.php
+++ src/UserBundle/Tests/Controller/UserControllerTest.php
@@ -6,11 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    public function setUp()
+    {
+    }
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/api/v1/users');
 
         $this->assertContains('Hello World', $client->getResponse()->getContent());
     }
